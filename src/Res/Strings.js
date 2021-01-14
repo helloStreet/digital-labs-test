e5202626e3458f49343f927f0f6146e48c197506
Chris Street <chrisstreet58@gmail.com>
2021-01-14 16:11:34 +0000

    Spell Checked

--- src/Res/Strings.js
+++ src/Res/Strings.js
@@ -3,6 +3,6 @@
 
 export const Recipe1 = 'Portobello Baked Eggs with avocado & bread';
 export const Recipe2 = 'Summery salmon, beetroot & spicy mix';
-export const Recipe3 = 'Backed Avocado Egg with herbs';
+export const Recipe3 = 'Baked Avocado Egg with herbs';
 
 
